330f0c2f26c8ff5e2737674c5f4c400161d5c3f4
tiangan10 <gant.facebook@gmail.com>
2017-03-09 00:42:56 -0500

    using jquery

--- tweetme/settings/base.py
+++ tweetme/settings/base.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
 
     'crispy_forms',
     'rest_framework',
-    
+
     'tweets',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,12 +1,26 @@
 from rest_framework import generics
-
+from rest_framework import permissions
+from django.db.models import Q
 
 from tweets.models import Tweet
 from .serializers import TweetModelSerializer
 
+class TweetCreateAPIView(generics.CreateAPIView):
+	serializer_class = TweetModelSerializer
+	permission_classes = [permissions.IsAuthenticated]
+	def perform_create(self, serializer):
+		serializer.save(user = self.request.user)
 
 class TweetListAPIView(generics.ListAPIView):
-    serializer_class = TweetModelSerializer
+	serializer_class = TweetModelSerializer
 
-    def get_queryset(self):
-        return Tweet.objects.all()
+	def get_queryset(self, *args, **kwargs):
+		qs = Tweet.objects.all().order_by('-timestamp')
+		print(self.request.GET)
+		query = self.request.GET.get("q", None)
+		if query is not None:
+			qs = qs.filter(
+					Q(content__icontains=query) |
+					Q(user__username__icontains=query)
+				)
+		return qs
